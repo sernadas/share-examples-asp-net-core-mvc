393f0fa61af31e11f59c4cb8afc9a2c7dbaed344
sernadas <sernadas@gmail.com>
2019-03-11 16:05:30 +0000

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -43,6 +44,8 @@ namespace SalesWebMvc
                 builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+
+            services.AddScoped<SellerService>(); 
 
         }
 
