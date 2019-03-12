322de4d87023783e83e93f636e1e878a01fbdabf
sernadas <sernadas@gmail.com>
2019-03-12 12:34:44 +0000

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -23,13 +23,24 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById (int id)
+        {
+            //return _context.Seller.Where(s => s.Id == id).First();
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
         public void Insert (Seller obj)
         {
             _context.Seller.Add(obj);
             _context.SaveChanges();
         }
 
-
+        public void Remove(int id)
+        {
+            Seller obj = FindById(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
 
     }
 }
